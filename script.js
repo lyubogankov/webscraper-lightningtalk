17cc53d0712ff740867fb098e20d8ccca70e9131
lyubogankov <lyubogankov13@gmail.com>
2023-03-21 07:53:03 -0700

    updated demo page to not have the hidden content initially - on first click, it's populated from the script (same mechanism as if it were from server -- still via script)

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+// obtain references to the visible rows' description text
 const descriptions = document.querySelectorAll('td.description')
+// obtain references to all rows (visible and invisible)
 const allrows = document.querySelectorAll('tbody tr')
 
-// register event listeners to each description, for the row below it
+// hidden content
+const payment_methods = ['Online', 'Online', 'Online', 'In-person', 'In-person', 'In-person', 'In-person'];
 
+// register event listeners to each visible description, for the hidden row below it
 const clickCounts = Array(descriptions.length).fill(0);
 
 for(let i=0; i < descriptions.length; i++) {
     descriptions[i].addEventListener(
         "click",
         () => {
+            togglerow = allrows[i*2 + 1];
+            togglerowtext = togglerow.children[1].innerText;
+
+            // populate content onto page the first time the description is clicked
+            // (not sure why, but it wasn't updating using togglerowtext, had to )
+            if (togglerowtext === "")
+                togglerow.children[1].innerText = payment_methods[i];
+
+            // toggle visibility
             if (clickCounts[i] % 2 === 0)
-                allrows[i*2 + 1].classList.remove("hidden");
+                togglerow.classList.remove("hidden");
             else
-                allrows[i*2 + 1].classList.add("hidden");
+                togglerow.classList.add("hidden");
             clickCounts[i] += 1;
         }
     );
