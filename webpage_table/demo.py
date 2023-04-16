64fbf501edf134b961fd5a61f287068312ebce09
lyubogankov <lyubogankov13@gmail.com>
2023-04-16 16:43:22 -0700

    updating media folder with latest gif

--- webpage_table/demo.py
+++ webpage_table/demo.py
@@ -9,11 +9,9 @@ def demo_table():
             pyautogui.click()
 
 def demo_woe():
-    for x, y, duration, delay_after in [( 980, 400, 1, 3),  # inspector: html table row Q1 expand
-                                        ( 980, 400, 0, 1),  # inspector: html table row Q1 collapse
-                                        ( 980, 440, 1, 2),  # inspector: html table row A1 expand
-                                        ( 343, 272, 2, 2),  # webpage:   click on Q1 row 
-                                        (1013, 481, 2, 5)]: # inspector: html table row Q1 content expand
+    time.sleep(7)
+    for x, y, duration, delay_after in [( 343, 272, 2, 2),  # webpage:   click on Q1 row 
+                                        (1013, 561, 2, 5)]: # inspector: html table row Q1 content expand
         pyautogui.moveTo(x=x, y=y, duration=duration, tween=pyautogui.easeOutQuad)  # start fast, end slow
         pyautogui.click()
         time.sleep(delay_after)
